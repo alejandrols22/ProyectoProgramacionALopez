5a366beed4d7f474b1af70905b59d48238171990
alejandrols22 <alejandrolopsep@gmail.com>
2023-05-29 13:06:25 +0200

    diesño de PantallaActualizarDatos completado
    Diseño ya completado, solo falta la funcionalidad de la actualizacion de datos

--- alopezproyecto1daw.3trimestre/src/main/java/interfaces/PantallaActualizarDatos.java
+++ alopezproyecto1daw.3trimestre/src/main/java/interfaces/PantallaActualizarDatos.java
@@ -1,121 +1,224 @@
 package interfaces;
 
 import javax.swing.*;
-import utilidad.DAO;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.RoundRectangle2D;
 import java.sql.SQLException;
 import java.util.HashMap;
 
+import enums.NivelActividad;
+import enums.ObjetivoUsuario;
+
 public class PantallaActualizarDatos extends JFrame {
+
     JTextField telefonoField;
-    JPasswordField contrasenaField;
+    private JTextField contrasenaField;
     JTextField pesoField;
     JTextField alturaField;
-    JTextField sexoField;
-    JTextField nivelActividadField;
-    JTextField objetivoField;
-    JTextField objetivoCaloriasField;
+    JComboBox<Character> sexoField;
+    JComboBox<NivelActividad> nivelActividadField;
+    JComboBox<ObjetivoUsuario> objetivoField;
+    JSpinner spinnerObjetivoCaloriasField;
+    protected JButton actualizarButton;
+    protected JButton botonVolver;
 
     public PantallaActualizarDatos() {
-        setSize(400, 600);
-        setLayout(null);
-        setLocationRelativeTo(null);
+        // Configuración inicial de la ventana
+        setTitle("Actualizar Datos");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        getContentPane().setBackground(new Color(50, 50, 50));
 
+        // Paneles para la organización de los componentes
+        JPanel contentPanel = new JPanel(new GridBagLayout());
+        contentPanel.setBackground(new Color(50, 50, 50));
+        contentPanel.setBorder(BorderFactory.createEmptyBorder(300, 500, 300, 500));
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(20, 20, 20, 20); // increased padding for larger components
+
+        // Configuración de los componentes
         JLabel telefonoLabel = new JLabel("Telefono:");
-        telefonoLabel.setBounds(100, 60, 80, 30);
-        getContentPane().add(telefonoLabel);
+        telefonoLabel.setForeground(Color.WHITE);
+        telefonoLabel.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
+        telefonoField = new JTextField(20);
+        telefonoField.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
 
-        telefonoField = new JTextField();
-        telefonoField.setBounds(200, 60, 120, 30);
-        getContentPane().add(telefonoField);
-
-        JLabel contrasenaLabel = new JLabel("Contrasena:");
-        contrasenaLabel.setBounds(100, 100, 80, 30);
-        getContentPane().add(contrasenaLabel);
-
-        contrasenaField = new JPasswordField();
-        contrasenaField.setBounds(200, 100, 120, 30);
-        getContentPane().add(contrasenaField);
+        JLabel contrasenaLabel = new JLabel("Contraseña:");
+        contrasenaLabel.setForeground(Color.WHITE);
+        contrasenaLabel.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
+        contrasenaField = new JTextField(20);
+        contrasenaField.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
 
         JLabel pesoLabel = new JLabel("Peso:");
-        pesoLabel.setBounds(100, 140, 80, 30);
-        getContentPane().add(pesoLabel);
-
-        pesoField = new JTextField();
-        pesoField.setBounds(200, 140, 120, 30);
-        getContentPane().add(pesoField);
+        pesoLabel.setForeground(Color.WHITE);
+        pesoLabel.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
+        pesoField = new JTextField(20);
+        pesoField.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
 
         JLabel alturaLabel = new JLabel("Altura:");
-        alturaLabel.setBounds(100, 180, 80, 30);
-        getContentPane().add(alturaLabel);
-
-        alturaField = new JTextField();
-        alturaField.setBounds(200, 180, 120, 30);
-        getContentPane().add(alturaField);
+        alturaLabel.setForeground(Color.WHITE);
+        alturaLabel.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
+        alturaField = new JTextField(20);
+        alturaField.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
 
         JLabel sexoLabel = new JLabel("Sexo:");
-        sexoLabel.setBounds(100, 220, 80, 30);
-        getContentPane().add(sexoLabel);
-
-        sexoField = new JTextField();
-        sexoField.setBounds(200, 220, 120, 30);
-        getContentPane().add(sexoField);
+        sexoLabel.setForeground(Color.WHITE);
+        sexoLabel.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
+        sexoField = new JComboBox<>();
+        sexoField.addItem('h');
+        sexoField.addItem('m');
+        sexoField.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
 
         JLabel nivelActividadLabel = new JLabel("Nivel Actividad:");
-        nivelActividadLabel.setBounds(100, 260, 100, 30);
-        getContentPane().add(nivelActividadLabel);
-
-        nivelActividadField = new JTextField();
-        nivelActividadField.setBounds(200, 260, 120, 30);
-        getContentPane().add(nivelActividadField);
+        nivelActividadLabel.setForeground(Color.WHITE);
+        nivelActividadLabel.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
+        nivelActividadField = new JComboBox<>(NivelActividad.values());
+        nivelActividadField.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
 
         JLabel objetivoLabel = new JLabel("Objetivo:");
-        objetivoLabel.setBounds(100, 300, 80, 30);
-        getContentPane().add(objetivoLabel);
+        objetivoLabel.setForeground(Color.WHITE);
+        objetivoLabel.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
+        objetivoField = new JComboBox<>(ObjetivoUsuario.values());
+        objetivoField.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
 
-        objetivoField = new JTextField();
-        objetivoField.setBounds(200, 300, 120, 30);
-        getContentPane().add(objetivoField);
+        JLabel objetivoCaloriasLabel = new JLabel("Objetivo Calorías:");
+        objetivoCaloriasLabel.setForeground(Color.WHITE);
+        objetivoCaloriasLabel.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
+        spinnerObjetivoCaloriasField = new JSpinner(new SpinnerNumberModel(2000, 500, 4000, 100));
+        spinnerObjetivoCaloriasField.setFont(new Font("Arial", Font.PLAIN, 30)); // bigger font
 
-        JLabel objetivoCaloriasLabel = new JLabel("Objetivo Calorias:");
-        objetivoCaloriasLabel.setBounds(100, 340, 120, 30);
-        getContentPane().add(objetivoCaloriasLabel);
+        actualizarButton = createButton("Actualizar");
+        botonVolver = createButton("Volver");
 
-        objetivoCaloriasField = new JTextField();
-        objetivoCaloriasField.setBounds(200, 340, 120, 30);
-        getContentPane().add(objetivoCaloriasField);
+        c.weightx = 1.0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        // Añadiendo los componentes al panel
+        c.gridx = 0; c.gridy = 0;
+        contentPanel.add(telefonoLabel, c);
 
-        JButton actualizarButton = new JButton("Actualizar");
-        actualizarButton.setBounds(140, 380, 120, 30);
-        getContentPane().add(actualizarButton);
-        actualizarButton.addActionListener(e -> {
-            HashMap<String, Object> datosAModificar = new HashMap<>();
-            datosAModificar.put("telefono", telefonoField.getText());
-            datosAModificar.put("contrasena", new String(contrasenaField.getPassword()));
-            datosAModificar.put("peso", pesoField.getText());
-            datosAModificar.put("altura", alturaField.getText());
-            datosAModificar.put("sexo", sexoField.getText());
-            datosAModificar.put("nivelActividad", nivelActividadField.getText());
-            datosAModificar.put("objetivo", objetivoField.getText());
-            datosAModificar.put("objetivoCalorias", objetivoCaloriasField.getText());
+        c.gridx = 1; c.gridy = 0;
+        contentPanel.add(telefonoField, c);
 
-            HashMap<String, Object> restricciones = new HashMap<>();
-            // Aquí tendrás que especificar las restricciones de la actualización.
-            // restricciones.put("nombre_actual", "valor_actual");
+        c.gridx = 0; c.gridy = 1;
+        contentPanel.add(contrasenaLabel, c);
 
-            try {
-                DAO.actualizar("nombre_de_tu_tabla", datosAModificar, restricciones);
-            } catch (SQLException ex) {
-                ex.printStackTrace();
+        c.gridx = 1; c.gridy = 1;
+        contentPanel.add(contrasenaField, c);
+
+        c.gridx = 0; c.gridy = 2;
+        contentPanel.add(pesoLabel, c);
+
+        c.gridx = 1; c.gridy = 2;
+        contentPanel.add(pesoField, c);
+
+        c.gridx = 0; c.gridy = 3;
+        contentPanel.add(alturaLabel, c);
+
+        c.gridx = 1; c.gridy = 3;
+        contentPanel.add(alturaField, c);
+
+        c.gridx = 0; c.gridy = 4;
+        contentPanel.add(sexoLabel, c);
+
+        c.gridx = 1; c.gridy = 4;
+        contentPanel.add(sexoField, c);
+
+        c.gridx = 0; c.gridy = 5;
+        contentPanel.add(nivelActividadLabel, c);
+
+        c.gridx = 1; c.gridy = 5;
+        contentPanel.add(nivelActividadField, c);
+
+        c.gridx = 0; c.gridy = 6;
+        contentPanel.add(objetivoLabel, c);
+
+        c.gridx = 1; c.gridy = 6;
+        contentPanel.add(objetivoField, c);
+
+        c.gridx = 0; c.gridy = 7;
+        contentPanel.add(objetivoCaloriasLabel, c);
+
+        c.gridx = 1; c.gridy = 7;
+        contentPanel.add(spinnerObjetivoCaloriasField, c);
+
+        c.gridx = 0; c.gridy = 8;
+        contentPanel.add(actualizarButton, c);
+
+        c.gridx = 1; c.gridy = 8;
+        contentPanel.add(botonVolver, c);
+
+        // Añadir el panel a la ventana
+        this.add(contentPanel, BorderLayout.CENTER);
+
+        // Listener para el botón de actualizar
+        actualizarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                HashMap<String, Object> datosAModificar = new HashMap<>();
+                datosAModificar.put("telefono", telefonoField.getText());
+                datosAModificar.put("contrasena", contrasenaField.getText());
+                datosAModificar.put("peso", pesoField.getText());
+                datosAModificar.put("altura", alturaField.getText());
+                datosAModificar.put("sexo", sexoField.getSelectedItem());
+                datosAModificar.put("nivelActividad", nivelActividadField.getSelectedItem());
+                datosAModificar.put("objetivo", objetivoField.getSelectedItem());
+                datosAModificar.put("objetivoCalorias", spinnerObjetivoCaloriasField.getValue());
+
+                HashMap<String, Object> restricciones = new HashMap<>();
+                // Aquí tendrás que especificar las restricciones de la actualización.
+                // restricciones.put("nombre_actual", "valor_actual");
+
+                try {
+                    DAO.actualizar("nombre_de_tu_tabla", datosAModificar, restricciones);
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
 
-        JButton botonVolver = new JButton("Volver");
-        botonVolver.setBounds(140, 420, 120, 30);
-        getContentPane().add(botonVolver);
-        botonVolver.addActionListener(e -> {
-            new PantallaPrincipal().setVisible(true);
-            this.setVisible(false);
+        // Listener para el botón de "Volver"
+        botonVolver.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PantallaPrincipal pantallaPrincipal = new PantallaPrincipal();
+                pantallaPrincipal.setVisible(true);
+                dispose(); // Cerrar la ventana actual de registro
+            }
         });
     }
+
+    protected JButton createButton(String text) {
+        JButton button = new CustomButton(text);
+        button.setFont(new Font("Arial", Font.BOLD, 35)); // bigger font
+        button.setBackground(Color.WHITE);
+        button.setForeground(Color.BLACK);
+        return button;
+    }
+
+    private class CustomButton extends JButton {
+        public CustomButton(String text) {
+            super(text);
+            setOpaque(false);
+            setContentAreaFilled(false);
+            setFocusPainted(false);
+            setBorderPainted(false);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            if (getModel().isPressed()) {
+                g.setColor(new Color(200, 200, 200));
+            } else {
+                g.setColor(getBackground());
+            }
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.fill(new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), 15, 15));
+            super.paintComponent(g);
+        }
+    }
 }
+
